b6a08fa4d67853ecbdd564fab3a3e94e3ba24069
Taras <kolyada.taras42@gmail.com>
2023-06-06 21:45:33 +0300

    Add 'Get' and 'Post' methods to 'Delete' functionality

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -58,5 +58,17 @@ namespace BookWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Delete(int id) 
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var categoryToEdit = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryToEdit == null)
+                return NotFound();
+
+            return View(categoryToEdit);
+        }
     }
 }
