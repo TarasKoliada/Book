62dfc4a9327bcf08eddefef0cdff734c62807cfb
Taras <kolyada.taras42@gmail.com>
2023-06-06 18:16:12 +0300

    Change bootstrap layout and make category output in view

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categories = _context.Categories.ToList();
+            return View(categories);
         }
     }
 }
