8b15736fa9ae5981fa7c76d9ae33e0d645010ead
Taras <kolyada.taras42@gmail.com>
2023-06-17 15:22:25 +0300

    Add CategoryRepository

---
+++ BookWeb.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookWeb.DataAccess.Data;
+using BookWeb.DataAccess.Repository.IRepository;
+using BookWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWeb.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context) : base(context) { _context = context; }
+        public void Save() => _context.SaveChanges();
+
+        public void Update(Category category) => _context.Update(category);
+    }
+}
