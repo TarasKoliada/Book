5adf3834ffc0d4b4a0eb057e0bbbe060f38d0b3a
Taras <kolyada.taras42@gmail.com>
2023-07-20 15:13:51 +0300

    Create Roles to DataBase table

--- BookWeb.Utility/StaticDetails.cs
+++ BookWeb.Utility/StaticDetails.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace BookWeb.Utility
 {
-    internal class StaticDetails
+    public class StaticDetails
     {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
     }
 }
--- BookWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,6 +11,7 @@ using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
 using BookWeb.Models;
+using BookWeb.Utility;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -26,6 +27,7 @@ namespace BookWeb.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IUserStore<IdentityUser> _userStore;
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
@@ -33,12 +35,14 @@ namespace BookWeb.Areas.Identity.Pages.Account
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
             _userStore = userStore;
             _emailStore = GetEmailStore();
             _signInManager = signInManager;
@@ -103,6 +107,14 @@ namespace BookWeb.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+            if (! await _roleManager.RoleExistsAsync(StaticDetails.Role_Customer))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Customer));
+                await _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Admin));
+                await _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Employee));
+                await _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Company));
+            }
+
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
