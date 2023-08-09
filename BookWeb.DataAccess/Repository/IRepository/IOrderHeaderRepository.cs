a2a5e56688929be9282fb0b5beec4cfdb2181d65
Taras <kolyada.taras42@gmail.com>
2023-08-09 23:25:05 +0300

    Add helper methods in OrderHeaderRepository to update Order Status and Payment Id

--- BookWeb.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ BookWeb.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -5,5 +5,7 @@ namespace BookWeb.DataAccess.Repository.IRepository
     public interface IOrderHeaderRepository : IRepository<OrderHeader>
     {
         void Update(OrderHeader orderHeader);
+        void UpdateStatus(int orderHeaderId, string orderStatus, string? paymentStatus = null);
+        void UpdateStripePaymentId(int orderHeaderId, string sessionId, string paymentIntentId);
     }
 }
--- BookWeb.DataAccess/Repository/OrderHeaderRepository.cs
+++ BookWeb.DataAccess/Repository/OrderHeaderRepository.cs
@@ -18,5 +18,36 @@ namespace BookWeb.DataAccess.Repository
         }
 
         public void Update(OrderHeader orderHeader) => _context.OrderHeaders.Update(orderHeader);
-    }
+
+		public void UpdateStatus(int orderHeaderId, string orderStatus, string? paymentStatus = null)
+		{
+			var orderHeader = _context.OrderHeaders.FirstOrDefault(oh => oh.Id == orderHeaderId);
+			if (orderHeader != null) 
+			{
+				orderHeader.OrderStatus = orderStatus;
+
+				if (!string.IsNullOrEmpty(paymentStatus))
+					orderHeader.PaymentStatus = paymentStatus;
+			}
+		}
+
+		public void UpdateStripePaymentId(int orderHeaderId, string sessionId, string paymentIntentId)
+		{
+			var orderHeader = _context.OrderHeaders.FirstOrDefault(oh => oh.Id == orderHeaderId);
+
+			if (orderHeader != null)
+			{
+				//sessionId generates when the user tries to make a payment, and if it successfull - generates paymentIntentId
+				if (!string.IsNullOrEmpty(sessionId))
+					orderHeader.SessionId = sessionId;
+
+				if (!string.IsNullOrEmpty(paymentIntentId))
+				{
+					orderHeader.PaymentIntentId = paymentIntentId;
+					orderHeader.PaymentDate = DateTime.Now;
+				}
+
+			}
+		}
+	}
 }
