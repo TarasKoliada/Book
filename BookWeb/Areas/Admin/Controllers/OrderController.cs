bb35b3bd52a9148bf1549ed91626241f3204e72f
Taras <kolyada.taras42@gmail.com>
2023-09-06 13:16:48 +0300

    Add OrderController and OrderVM

---
+++ BookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using BookWeb.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWeb.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var orders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = orders });
+		}
+		#endregion
+	}
+}
