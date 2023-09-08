82c30b8ae828c3e4d90c5b81ba67935380f50e96
Taras <kolyada.taras42@gmail.com>
2023-09-08 14:11:58 +0300

    Provide the button "Update Order Details" logic

--- BookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using BookWeb.Models;
 using BookWeb.Models.ViewModels;
 using BookWeb.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +11,8 @@ namespace BookWeb.Areas.Admin.Controllers
 	[Area("admin")]
 	public class OrderController : Controller
 	{
+		[BindProperty]
+		public OrderVM OrderVm { get; set; }
 		private readonly IUnitOfWork _unitOfWork;
         public OrderController(IUnitOfWork unitOfWork)
         {
@@ -21,12 +24,38 @@ namespace BookWeb.Areas.Admin.Controllers
 		}
 		public IActionResult Details(int orderId) 
 		{
-			OrderVM orderVm = new()
+			OrderVm = new()
 			{
 				OrderHeader = _unitOfWork.OrderHeader.Get(orderHeader => orderHeader.Id == orderId, includeProperties: "ApplicationUser"),
 				OrderDetail = _unitOfWork.OrderDetail.GetAll(orderDetail => orderDetail.OrderHeaderId == orderId, includeProperties: "Product")
 			};
-			return View(orderVm);
+			return View(OrderVm);
+		}
+
+		[HttpPost]
+		[Authorize(Roles = StaticDetails.Role_Admin +","+ StaticDetails.Role_Employee)]
+		public IActionResult UpdateOrderDetails()
+		{
+			var orderToUpdate = _unitOfWork.OrderHeader.Get(o => o.Id == OrderVm.OrderHeader.Id);
+			orderToUpdate.Name = OrderVm.OrderHeader.Name;
+			orderToUpdate.PhoneNumber = OrderVm.OrderHeader.PhoneNumber;
+			orderToUpdate.StreetAddress = OrderVm.OrderHeader.StreetAddress;
+			orderToUpdate.City = OrderVm.OrderHeader.City;
+			orderToUpdate.State = OrderVm.OrderHeader.State;
+			orderToUpdate.PostalCode = OrderVm.OrderHeader.PostalCode;
+
+			if (!string.IsNullOrEmpty(OrderVm.OrderHeader.Carrier))
+				orderToUpdate.Carrier = OrderVm.OrderHeader.Carrier;
+
+			if (!string.IsNullOrEmpty(OrderVm.OrderHeader.TrackingNumber))
+				orderToUpdate.TrackingNumber = OrderVm.OrderHeader.TrackingNumber;
+
+			_unitOfWork.OrderHeader.Update(orderToUpdate);
+			_unitOfWork.Save();
+
+			TempData["Success"] = "Order Details Updated Successfully";
+
+			return RedirectToAction(nameof(Details), new { orderId = orderToUpdate.Id});
 		}
 
 		#region API CALLS
