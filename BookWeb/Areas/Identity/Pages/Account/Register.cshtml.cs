500d0cef0a9a5b3b8346223d798548a1b905cd87
Taras <kolyada.taras42@gmail.com>
2023-09-12 15:33:23 +0300

    Update Registration form. Add User creating by Admin

--- BookWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -200,7 +200,11 @@ namespace BookWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(StaticDetails.Role_Admin))
+                            TempData["success"] = "New User Created Successfully";
+                        else
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+
                         return LocalRedirect(returnUrl);
                     }
                 }
